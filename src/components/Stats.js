25c183b4f281ba34bf947ac375c401b76064711b
ishowdarkside <ajdin.omerovicc.ets@gmail.com>
2023-06-18 00:46:22 +0200

    initv4

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -3,7 +3,7 @@ export default function Stats({ tasks }) {
   const tasksComplete = tasks.filter((el) => el.complete).length;
   return (
     <footer className="stats">
-      {tasks.length > 0 && tasksComplete !== tasks.length ? (
+      {tasks.length > 0 && tasksComplete < tasks.length ? (
         <>
           <p>Total Tasks: {tasks.length} ✔</p>
           <p>In progress: {tasksInProgress} 🕔</p>
@@ -12,7 +12,9 @@ export default function Stats({ tasks }) {
       ) : (
         <h1>Create your List 😁</h1>
       )}
-      {tasks.length === tasksComplete ? <h1>You are good to go!👍</h1> : null}
+      {tasks.length > 0 && tasks.length === tasksComplete ? (
+        <h1>You are good to go!👍</h1>
+      ) : null}
     </footer>
   );
 }
